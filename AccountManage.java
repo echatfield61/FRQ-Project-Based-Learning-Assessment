909a6261deac6b97fa0964c83df33071504bf2e7
ZongyiWan <157513609+ZongyiWan@users.noreply.github.com>
2024-02-08 13:15:06 -0500

    Add files via upload

---
+++ AccountManage.java
@@ -0,0 +1,21 @@
+package dining_management;
+import java.util.Map;
+import java.util.Set;
+import java.util.List;
+
+public class AccountManage {
+    private List<StudentAccount> roster;
+    public AccountManage(List<StudentAccount> roster) {
+        this.roster = roster;
+    }
+
+    public List<Meal> mostPopularDishes(List<Meal> menu){
+        
+    }
+
+    public double averageAmountSpent() {
+
+    }
+
+    public double
+}
